e65abb5d121161043d5a0d8c1e502c339a997398
HenriqueCsar <60591111+HenriqueCsar@users.noreply.github.com>
2021-04-07 20:20:54 -0300

    Add files via upload

---
+++ brs-go.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+//Basic system of cadastra
+
+func main() {
+	f, err := os.Create("dados.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+	for i := 1; i > 0; i++ {
+		var (
+			nome  string
+			idade string
+		)
+
+		fmt.Println("Seu nome:")
+		fmt.Scanln(&nome)
+		fmt.Println("Sua idade:")
+		fmt.Scanln(&idade)
+
+		_, err2 := f.WriteString("\n Nome: " + nome + " Idade: " + idade)
+
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	}
+}
